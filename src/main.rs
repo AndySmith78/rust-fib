7ba5d3a2e902349fa67d2ff7d6728ed5edac1e1b
Andy Smith <smith780@gmail.com>
2015-03-14 14:59:52 -0400

    Allows input for Fib sequence
    You can now enter a number to which you want to display the sequence
    for.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-fn main() {
+use std::old_io;
 
-  generate_sequence();
+fn main() {
+  let mut reader = std::old_io::stdin();
+  println!("How far on the Fibanoci do you want to display: ");
+  let input = reader.read_line().ok().expect("Failed to Read");
+  let input_num: Result<isize, _> = input.trim().parse();
+  let num = match input_num {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Please input a number!");
+            return;
+        }
+    };
+  generate_sequence(num);
   
 }
 
-fn generate_sequence() {  
-  let mut almost_last_num = 0i32;
-  let mut last_num = 1i32;  
+
+
+
+fn generate_sequence(input: isize) {  
+  let mut almost_last_num: isize = 0;
+  let mut last_num: isize = 1;  
 
   println!("{}", almost_last_num);
   println!("{}", last_num);
   
 
-  for x in 0..10 {
+  for x in 0..input {
     let next_num = last_num + almost_last_num;
 
     println!("{}", next_num);
