e9eaba3a1b31d67d549d03fa1b25a81415037d0c
Andy Smith <smith780@gmail.com>
2015-03-23 12:26:45 -0400

    Refactors and removes some warnings
    Still gets warnings on some of the std::io stuff.  Need to migrate to
    the new version.

--- src/main.rs
+++ src/main.rs
@@ -13,30 +13,33 @@ fn start_fib() -> String {
   reader.read_line().ok().expect("Failed to Read")
 }
 
-fn string_to_number(input: String) -> isize {
-  let input_num: Result<isize, _> = input.trim().parse();
+fn string_to_number(input: String) -> i32 {
+  let input_num: Result<i32, _> = input.trim().parse();
    
   match input_num {
         Ok(num) => num,
         Err(_) => 0,
   }
 }
-fn generate_sequence(input: isize) {  
-  let mut almost_last_num: isize = 0;
-  let mut last_num: isize = 1;  
+fn generate_sequence(input: i32) {  
+  let almost_last_num: isize = 0;
+  let last_num: isize = 1;  
 
-  println!("{}", almost_last_num);
-  println!("{}", last_num);
+  print_number(1, almost_last_num);
+  print_number(2, last_num);
   
   fib_loop(input, last_num, almost_last_num);
 }
 
-fn fib_loop(mut input: isize, mut last_num: isize, mut almost_last_num: isize) {
-  for x in 0..input {
+fn fib_loop(input: i32, mut last_num: isize, mut almost_last_num: isize) {
+  for x in 3..input + 1{
     let next_num = last_num + almost_last_num;
-
-    println!("{}", next_num);
+    print_number(x, next_num);
     almost_last_num = last_num;
     last_num = next_num;
   }
+}
+
+fn print_number(input: i32, fib_number: isize) {
+  println!("Digit number {} is: {}", input, fib_number);
 }
